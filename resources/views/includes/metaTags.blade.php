cade1bb38dcc3ef0015035a69a3ec4f79e0ba49c
Tyler <tylereadams@gmail.com>
2019-05-20 20:32:49 -0400

    Add default description.

--- resources/views/includes/metaTags.blade.php
+++ resources/views/includes/metaTags.blade.php
@@ -1,11 +1,11 @@
 <meta property="og:title" content="{{ isset($title) ? $title : 'Find Highlights' }}">
-<meta property="og:description" content="{{ isset($description) ? $description : ''}}">
+<meta property="og:description" content="{{ isset($description) ? $description : 'Search for highlights of games, players or teams. Free & simple, no login necessary'}}">
 <meta property="og:image" content="{{ isset($imageUrl) ? $imageUrl : '' }}">
 <meta property="og:url" content="{{ isset($url) ? $url : url()->current() }}">
 <meta property="og:type" content="website" />
 
 
 <meta name="twitter:title" content="{{ isset($title) ? $title : 'Find Highlights' }}">
-<meta name="twitter:description" content="{{ isset($description) ? $description : '' }}">
+<meta name="twitter:description" content="{{ isset($description) ? $description : 'Search for highlights of games, players or teams. Free & simple, no login necessary.' }}">
 <meta name="twitter:image" content="{{ isset($imageUrl) ? $imageUrl : '' }}">
 <meta name="twitter:card" content="summary_large_image">
